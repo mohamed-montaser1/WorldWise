9a24d50fefc7c1c6a4ca5be304ca6313f9981409
mohamed montaser <mastrocoding@gmail.com>
2024-10-19 10:48:55 +0300

    setup basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
 
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
