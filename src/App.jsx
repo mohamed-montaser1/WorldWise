0e242a379741b37aef1ddacab46ef06b3c6a1f47
mohamed montaser <mastrocoding@gmail.com>
2024-10-19 14:31:53 +0300

    finish (home, product, pricing, login) pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Homepage from "./pages/Homepage";
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage/Homepage";
+import Product from "./pages/Product/Product";
+import Pricing from "./pages/Pricing/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login/Login";
+import AppLayout from "./pages/AppLayout/AppLayout";
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import Logo from "../Logo/Logo";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to={"/"}>Home</NavLink>
-        </li>
         <li>
           <NavLink to={"/pricing"}>Pricing</NavLink>
         </li>
         <li>
           <NavLink to={"/product"}>Product</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/login"} className={styles.ctaLink}>
+            login
+          </NavLink>
         </li>
       </ul>
     </nav>
